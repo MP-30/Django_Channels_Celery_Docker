52358cfdb3c225388f55472707ea3e9eed3392b1
MP-30 <adityabhadauriya93@gmail.com>
2024-03-06 22:30:14 +0530

    channels

--- command.md
+++ command.md
@@ -1,5 +1,6 @@
 pip freeze > requirements.txt
 sudo chmod +x ./entrypoint.sh
-docker-compose up -d --build
+sudo docker-compose up -d --build
 ./manage.py startapp taskapp
 docker exec -it django /bin/sh
+docker rm -f $(docker ps -aq)
--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 #DEBUG = 1
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","").split(",")
 
 #ALLOWED_HOSTS = ['*']
 
